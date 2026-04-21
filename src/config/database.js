5d0892401908888712539e790db9a1200d14efd6
landerguevarra01 <guevarralander0@gmail.com>
2026-04-21 13:38:47 +0800

    include ssl

--- src/config/database.js
+++ src/config/database.js
@@ -7,13 +7,18 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  String(process.env.DB_PASSWORD || ""), // 🔥 force string
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    logging: process.env.NODE_ENV === "development" ? console.log : false,
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 );
 
 const connectDB = async () => {
